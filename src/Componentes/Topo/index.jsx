d08191209ceec7a3f305873dda1e687b4a44ad96
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-05-28 20:47:13 -0300

    Inserindo elementos no componente Topo

--- src/Componentes/Topo/index.jsx
+++ src/Componentes/Topo/index.jsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import styles from "./Topo.module.css";
+import Dente from "../../../public/assets/dente.png";
 
 export default function Topo () {
     return (
-        <>
-        </>
+        <div className={styles.topo}>
+            <div className={styles.container_logo}>
+                <Image src={Dente} alt="Dentes saudáveis" title="Dentes saudáveis" />
+                <p>Dentes saudáveis</p>
+            </div>
+        </div>
     )
 }

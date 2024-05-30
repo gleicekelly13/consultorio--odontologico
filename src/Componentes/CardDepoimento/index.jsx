1d66f7e5e8959cae7cadd41e6061de5043b51208
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-05-30 17:07:59 -0300

    Adicionando elementos no componente CardDepoimento e recebendo  props

--- src/Componentes/CardDepoimento/index.jsx
+++ src/Componentes/CardDepoimento/index.jsx
@@ -1,7 +1,16 @@
-export default function CardDepoimento () {
+import Image from "next/image"
+import styles from './CardDepomento.module.css'
+
+export default function CardDepoimento (props) {
     return (
-        <>
-            
-        </>
+        <div className={styles.card_depoimento}>
+            <Image className={styles.img_aparelho}
+                src={props.imagem}
+                alt={props.nome}
+                title={props.nome}
+            />
+            <h3>{props.nome}</h3>
+            <p>{props.descricao}</p>
+        </div>
     )
 }

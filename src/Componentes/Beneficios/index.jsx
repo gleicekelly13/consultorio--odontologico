559985117452c5e13c8ecab321edac5161dce455
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-05-30 15:48:21 -0300

    Adicionando lista no componente Beneficios

--- src/Componentes/Beneficios/index.jsx
+++ src/Componentes/Beneficios/index.jsx
@@ -13,6 +13,28 @@ export default function Beneficios () {
                     <Image src={Aparelho} alt="Imagem do uso de um aparelho" title="Imagem do uso de um aparelho" />
                 </figure>
             </div>
+
+            <ul>
+                <li>
+                    <h3>Alinhar os dentes</h3>
+                    <p>Dentes desalinhados causam problemas nos dentes e podem afetar a
+                digestão dos alimentos e a respiração.</p>
+                </li>
+
+                <li>
+                    <h3>Melhorar a dicção e a higiene dentária</h3>
+                    <p>Muitas pessoas não conseguem nem usar fio dental devido à posição
+                da sua dentição. Contudo, a correção possibilita o cuidado com a
+                saúde bucal de forma bem mais ampla.</p>
+                </li>
+
+                <li>
+                    <h3>Corrigir espaço entre os dentes</h3>
+                    <p>Uma dentição desalinhada e com espaços significativos incomodam
+                muitas pessoas. Usar aparelho nos dentes é uma das formas mais
+                eficientes para que esses problemas possam ser corrigidos.</p>
+                </li>
+            </ul>
         </section>
     )
 }

8eff262f9de96a038d111d96d32b590664773817
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-06-01 15:58:47 -0300

    Adicionando elementos no segundo card da Secao_informacoes

--- src/Componentes/Secao_informacoes/index.jsx
+++ src/Componentes/Secao_informacoes/index.jsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import Dentista03 from "../../../public/assets/medico03.png"
+import Dentista02 from "../../../public/assets/medico02.png"
 import styles from './Secao_informacoes.module.css'
 
 export default function Secao_informacoes () {
@@ -12,6 +15,33 @@ export default function Secao_informacoes () {
                     <li>Sexta - 9:00 às 17:00</li>
                     <li>Sábado - 9:00 às 12:00</li>
                 </ul>
+
+                <div className={styles.card}>
+                    <figure>
+                        <Image className={styles.img_aparelho}
+                            src={Dentista03}
+                            alt='Dra. Ana'
+                            title='Dra. Ana'
+                        />
+                    </figure>
+                    <div>
+                        <p>Dra. Ana - Ortodontista</p>
+                        <p>Segundas e Sextas</p>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                    <figure>
+                        <Image className={styles.img_aparelho}
+                            src={Dentista02}
+                            alt='Dr. Carlos'
+                            title='Dr. Carlos'
+                        />
+                    </figure>
+                    <div>
+                        <p>Dr. Carlos - Entodontia</p>
+                        <p>Terças e Quartas</p>
+                    </div>
+                </div>
             </div>
         </section>
     )

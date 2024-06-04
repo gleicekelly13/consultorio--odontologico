a323e34f3bc6e07d8622f4135bf5e98b60e7925c
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-06-04 17:33:25 -0300

    Adicionando comentários no componente Topo

--- src/Componentes/Topo/index.jsx
+++ src/Componentes/Topo/index.jsx
@@ -4,8 +4,8 @@ import Dente from "../../../public/assets/dente.png";
 
 export default function Topo () {
     return (
-        <div className={styles.topo}>
-            <div className={styles.container_logo}>
+        <div className={styles.topo}> {/* Área que engloba a área das logos */}
+            <div className={styles.container_logo}> {/* Área que engloba as logos */}
                 <Image src={Dente} alt="Dentes saudáveis" title="Dentes saudáveis" />
                 <p>Dentes saudáveis</p>
             </div>

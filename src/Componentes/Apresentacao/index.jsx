417a785a9de53e9215446f2f0f24c7c685f858d8
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-06-05 19:35:42 -0300

    Estlização do componente Apresentacao

--- src/Componentes/Apresentacao/index.jsx
+++ src/Componentes/Apresentacao/index.jsx
@@ -4,14 +4,14 @@ import styles from "./Apresentacao.module.css"
 
 export default function Apresentacao () {
     return (
-        <section className={styles.secao_apresentacao}>
-            <div className={styles.container_texto_apresentacao}>
+        <section className={styles.secao_apresentacao}> {/* Área que engloba a div e a lista */}
+            <div className={styles.container_texto_apresentacao}> {/* Área que engloba os textos */}
                 <h1>Os melhores <span>aparelhos dentários</span>!</h1>
                 <p>Confira abaixo todas as especialidades odontológicas que temos à sua
               disposição!</p>
             </div>
 
-            <ul className={styles.lista_servicos}>
+            <ul className={styles.lista_servicos}> {/* Área que engloba */}
                 <li>
                     <Image src={Dente} alt= "logo" title= "logo"/>
                     <p>Pré-avaliação</p>
